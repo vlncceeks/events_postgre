0d140ddec4606cf1d081b9c0adc87791a0efc22c
Ксения <vlncceeks@gmail.com>
2025-01-16 10:36:06 +0300

    проверка 3

--- main/pagination.py
+++ main/pagination.py
@@ -1,6 +1,6 @@
 from rest_framework.pagination import PageNumberPagination
 
 class CustomPagination(PageNumberPagination):
-    page_size = 5
+    page_size = 3
     page_size_query_param = 'page_size'  # Пользователь может указать размер страницы
     max_page_size = 50  # Максимальный размер страницы
